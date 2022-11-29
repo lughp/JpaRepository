f380ab1e8b0d4c17615932b88c1ff26d0304be14
Luciano Holanda Porto <lug.dba@gmail.com>
2022-11-29 12:08:02 -0300

    UserController - GetMapping

--- src/main/java/br/eti/lug/jparepository/controllers/UserController.java
+++ src/main/java/br/eti/lug/jparepository/controllers/UserController.java
@@ -3,9 +3,12 @@ package br.eti.lug.jparepository.controllers;
 import br.eti.lug.jparepository.entities.User;
 import br.eti.lug.jparepository.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +25,28 @@ public class UserController {
         List<User> result = repository.findAll();
         return ResponseEntity.ok(result);
     }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+        Page<User> result = repository.findAll(pageable);
+        return ResponseEntity.ok(result);
+    }
+    // http://localhost:8080/users/page?page=0&size=12&sort=name,salary,asc
+
+    @GetMapping(value = "/search-salary")
+    public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+        Page<User> result = repository.findBySalaryBetween(minSalary, maxSalary, pageable);
+        return ResponseEntity.ok(result);
+    }
+    // http://localhost:8080/users/search-salary?minSalary=9000
+    // http://localhost:8080/users/search-salary?maxSalary=8000&size=5
+    // http://localhost:8080/users/search-salary?minSalary=5000&maxSalary=10000
+
+    @GetMapping(value = "/search-name")
+    public ResponseEntity<Page<User>> searchByName(@RequestParam(defaultValue = "") String name, Pageable pageable) {
+        Page<User> result = repository.findByNameContainingIgnoreCase(name, pageable);
+        return ResponseEntity.ok(result);
+    }
+    // http://localhost:8080/users/search-name?name=maria
 
 }
--- src/main/java/br/eti/lug/jparepository/repositories/UserRepository.java
+++ src/main/java/br/eti/lug/jparepository/repositories/UserRepository.java
@@ -1,7 +1,15 @@
 package br.eti.lug.jparepository.repositories;
 
 import br.eti.lug.jparepository.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+    Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+
+    @Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
